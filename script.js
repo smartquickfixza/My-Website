f2913049b9012482f5517d8d9252436ac6961281
smartquickfixza <smartquickfixza@gmail.com>
2026-03-06 20:15:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleInfo(){
+
+let section = document.getElementById("info-section")
+
+if(section.style.display === "block"){
+section.style.display = "none"
+}
+else{
+section.style.display = "block"
+}
+
+}
